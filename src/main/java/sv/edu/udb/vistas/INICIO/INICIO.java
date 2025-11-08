ade7cc5cd9a0fc3aa7f8f2d91c9efc076a8ad188
Bryan Monterrosa <bryan_monterrosa@hotmail.com>
2025-11-08 17:54:30 -0600

    Arreglando imagenes en interfaz de logeo:
    - Se corrigieron las rutas de las imágenes en la clase INICIO.java para que apunten a la carpeta correcta "interfazInicio". Notar que cuando la imagen esta dentro de la carpeta "resources" de un proyecto Java, la ruta debe comenzar con una barra diagonal "/" seguida del nombre de la carpeta y el nombre del archivo de imagen.
    - Se eliminaron los archivos cambio.form y cambio.java que ya no son necesarios.
    - Se movieron las imágenes relacionadas con la interfaz de inicio a una subcarpeta llamada "interfazInicio" dentro de la carpeta "resources" para mantener una mejor organización.

--- src/main/java/sv/edu/udb/vistas/INICIO/INICIO.java
+++ src/main/java/sv/edu/udb/vistas/INICIO/INICIO.java
@@ -72,7 +72,7 @@ public class INICIO extends javax.swing.JFrame {
         txtInfo.setBorder(null);
         jScrollPane1.setViewportView(txtInfo);
 
-        icoLibro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sv/edu/udb/vistas/INICIO/libro-recomendado-icon.png"))); // NOI18N
+        icoLibro.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfazInicio/libro-recomendado-icon.png"))); // NOI18N
         icoLibro.setText("jLabel8");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -105,7 +105,7 @@ public class INICIO extends javax.swing.JFrame {
                                 .addContainerGap(184, Short.MAX_VALUE))
         );
 
-        icoLog.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sv/edu/udb/vistas/INICIO/UDB_horizontal22.png"))); // NOI18N
+        icoLog.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfazInicio/UDB_horizontal22.png"))); // NOI18N
         icoLog.setText("jLabel2");
 
         pwdContra.addActionListener(new java.awt.event.ActionListener() {
@@ -132,10 +132,10 @@ public class INICIO extends javax.swing.JFrame {
         lblBiblio.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
         lblBiblio.setText("Colegio Amigos De Don Bosco");
 
-        icoContra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sv/edu/udb/vistas/INICIO//WhatsApp Image 2025-11-06 at 7.56.38 AM.jpeg"))); // NOI18N
+        icoContra.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfazInicio/WhatsApp Image 2025-11-06 at 7.56.38 AM.jpeg"))); // NOI18N
         icoContra.setText("jLabel9");
 
-        icoCorreo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sv/edu/udb/vistas/INICIO//WhatsApp Image 2025-11-06 at 7.56.38 AM (1).jpeg"))); // NOI18N
+        icoCorreo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/interfazInicio/WhatsApp Image 2025-11-06 at 7.56.38 AM (1).jpeg"))); // NOI18N
         icoCorreo.setText("jLabel10");
 
         btnIngresar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
